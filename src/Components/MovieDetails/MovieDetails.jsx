6d6be7765e823668324ea9981ec6ecad54157711
Mohammed alibrahim <muhammed.94.alibrahim@gmail.com>
2025-04-28 19:23:49 +0300

    Add video trailer functionality and improve MovieDetails layout

--- src/Components/MovieDetails/MovieDetails.jsx
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -27,31 +27,83 @@ export default function MovieDetails() {
   useEffect(()=>{
     getApiTrending(mediaType , id)
   },[])
-  return <>
-  <div className="row">
-      <div className="col-md-4">
-        {details.poster_path ? <img src={'https://image.tmdb.org/t/p/w500'+details.poster_path} className='w-100' alt="poster path" />:<img src={'https://image.tmdb.org/t/p/w500'+details.profile_path} className='w-100' alt="poster path" />}
-      </div>
-      <div className="col-md-8 d-flex justify-content-center align-items-center">
-        <div>
-          <h2> {details.name} {details.title} </h2>
-          {details.overview ? <p> {details.overview} </p> :  <p> {details.biography} </p>}
-          {details.vote_average}
-        <div>
-          <button className='d-inline-block btn btn-info me-4 my-3'>play now</button>
-          <button
-            className="d-inline-block btn btn-info me-4 my-3"
-            onClick={(e) => {
-              e.stopPropagation(); // Prevent navigation when clicking the button
-                addToWatchlist(details);
-            }}
-          >
-            Add to Watchlist
-          </button>
-        </div>
+  const [videoKey, setVideoKey] = useState("");
 
+  async function getVideoKey(id) {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=09b639ab0b2b1b51b00568871d53f9fe`
+      );
+      const trailer = data.results.find(
+        (video) => video.type === "Trailer" && video.site === "YouTube"
+      );
+      if (trailer) {
+        setVideoKey(trailer.key);
+      }
+    } catch (error) {
+      console.error("Error fetching video key:", error);
+    }
+  }
+
+  useEffect(() => {
+    getVideoKey(id);
+  }, [id]);
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-4">
+          {details.poster_path ? (
+            <img
+              src={"https://image.tmdb.org/t/p/w500" + details.poster_path}
+              className="w-100"
+              alt="poster path"
+            />
+          ) : (
+            <img
+              src={"https://image.tmdb.org/t/p/w500" + details.profile_path}
+              className="w-100"
+              alt="poster path"
+            />
+          )}
+        </div>
+        <div className="col-md-8 d-flex justify-content-center align-items-center">
+          <div>
+            <h2>
+              {details.name} {details.title}
+            </h2>
+            {details.overview ? (
+              <p> {details.overview} </p>
+            ) : (
+              <p> {details.biography} </p>
+            )}
+            {details.vote_average}
+            <div>
+              <button className="d-inline-block btn btn-info me-4 my-3">
+                Play Now
+              </button>
+              <button
+                className="d-inline-block btn btn-info me-4 my-3"
+                onClick={() => {
+                  addToWatchlist(details);
+                }}
+              >
+                Add to Watchlist
+              </button>
+              {videoKey && (
+                <a
+                  href={`https://www.youtube.com/watch?v=${videoKey}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="d-inline-block btn btn-info me-4 my-3"
+                >
+                  Watch Trailer
+                </a>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-  </div>
     </>
+  );
 }
